e8eee32d7e455e3abcac24a2466ab62f6ebdb669
voovo0019-sudo <voovo0019@gmail.com>
2026-03-10 17:18:47 +0900

    Update main_tab_screen.dart to replace HomeScreenV44 with HomeScreen and pass locale parameters

--- lib/features/main_tab/main_tab_screen.dart
+++ lib/features/main_tab/main_tab_screen.dart
@@ -6,7 +6,7 @@
 
 import 'package:flutter/material.dart';
 import '../../core/app_localizations.dart';
-import 'package:lao_trust/screens/home_screen_v44.dart';
+import '../screens/home_screen.dart';
 import '../jobs/jobs_screen.dart';
 import '../chat/chat_screen.dart';
 import '../profile/profile_screen.dart';
@@ -39,11 +39,14 @@ class _MainTabScreenState extends State<MainTabScreen> {
     return Scaffold(
       body: IndexedStack(
         index: _currentIndex,
-        children: const [
-          HomeScreenV44(),
-          JobsScreen(),
-          ChatScreen(),
-          ProfileScreen(),
+        children: [
+          HomeScreen(
+            locale: widget.locale,
+            onLocaleChanged: widget.onLocaleChanged,
+          ),
+          const JobsScreen(),
+          const ChatScreen(),
+          const ProfileScreen(),
         ],
       ),
       bottomNavigationBar: Container(
